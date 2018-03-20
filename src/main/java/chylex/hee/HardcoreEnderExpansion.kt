cedb89d3c2658f465753ec348299fa34cb657d26
chylex <contact@chylex.com>
2018-03-20 08:27:16 +0100

    Store version in main class and add a network check

--- src/main/java/chylex/hee/HardcoreEnderExpansion.kt
+++ src/main/java/chylex/hee/HardcoreEnderExpansion.kt
@@ -4,6 +4,9 @@ import chylex.hee.proxy.ModCommonProxy
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent
+import net.minecraftforge.fml.common.network.NetworkCheckHandler
+import net.minecraftforge.fml.relauncher.Side
 import org.apache.logging.log4j.Logger
 
 @Mod(modid = HardcoreEnderExpansion.ID, useMetadata = true, modLanguageAdapter = "chylex.hee.system.core.KotlinAdapter", guiFactory = "chylex.hee.gui.ModGuiFactory")
@@ -11,11 +14,18 @@ object HardcoreEnderExpansion{
 	const val ID = "hee"
 	
 	lateinit var log: Logger
+	lateinit var version: String
 	lateinit var config: ModConfig
 	
     @EventHandler
 	fun onPreInit(e: FMLPreInitializationEvent){
 		log = e.modLog
+		version = e.modMetadata.version
 		config = ModConfig(e.suggestedConfigurationFile)
+	}
+	
+	@NetworkCheckHandler
+	fun onNetworkCheck(mods: Map<String, String>, @Suppress("UNUSED_PARAMETER") side: Side): Boolean{
+		return mods[ID] == version
 	}
 }
