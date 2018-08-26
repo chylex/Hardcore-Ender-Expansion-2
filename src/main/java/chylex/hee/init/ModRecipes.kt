238ad265b920b1d8684cd3eba32e0cb9310ff4b6
chylex <contact@chylex.com>
2018-08-26 16:34:38 +0200

    Add recipes for Gloomrock

--- src/main/java/chylex/hee/init/ModRecipes.kt
+++ src/main/java/chylex/hee/init/ModRecipes.kt
@@ -1,9 +1,11 @@
 package chylex.hee.init
+import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.FurnaceRecipes
 
 object ModRecipes{
 	fun initialize(){ // UPDATE: Move smelting recipes to JSON
 		with(FurnaceRecipes.instance()){
+			addSmeltingRecipeForBlock(ModBlocks.GLOOMROCK, ItemStack(ModBlocks.GLOOMROCK_SMOOTH), 0.1F)
 		}
 	}
 }
