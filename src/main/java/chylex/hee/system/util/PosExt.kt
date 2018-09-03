4409dd1491704b6aa4994e76672e920e29656246
chylex <contact@chylex.com>
2018-09-03 07:33:54 +0200

    Add a few extensions to PosExt & RandomExt

--- src/main/java/chylex/hee/system/util/PosExt.kt
+++ src/main/java/chylex/hee/system/util/PosExt.kt
@@ -65,7 +65,7 @@ inline fun BlockPos.breakBlock(world: World, drops: Boolean): Boolean{
 	return world.destroyBlock(this, drops)
 }
 
-// Calculations
+// Distance calculations
 
 fun BlockPos.distanceSqTo(x: Int, y: Int, z: Int): Double{
 	return (x - this.x).toDouble().pow(2) + (y - this.y).toDouble().pow(2) + (z - this.z).toDouble().pow(2)
@@ -82,3 +82,11 @@ fun BlockPos.distanceSqTo(entity: Entity): Double{
 inline fun BlockPos.distanceTo(x: Int, y: Int, z: Int): Double = sqrt(distanceSqTo(x, y, z))
 inline fun BlockPos.distanceTo(pos: BlockPos): Double = sqrt(distanceSqTo(pos))
 inline fun BlockPos.distanceTo(entity: Entity): Double = sqrt(distanceSqTo(entity))
+
+// Entity checks
+
+fun BlockPos.isAnyPlayerWithinRange(world: World, range: Double): Boolean{
+	return world.isAnyPlayerWithinRangeAt(x + 0.5, y + 0.5, z + 0.5, range)
+}
+
+inline fun BlockPos.isAnyPlayerWithinRange(world: World, range: Int): Boolean = isAnyPlayerWithinRange(world, range.toDouble())
