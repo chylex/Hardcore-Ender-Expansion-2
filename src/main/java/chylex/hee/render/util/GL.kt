db7df5388e3d002b619ab00d675e86a1c0b9a33e
chylex <contact@chylex.com>
2018-09-05 18:23:41 +0200

    Add minor math & GL utilities

---
+++ src/main/java/chylex/hee/render/util/GL.kt
@@ -0,0 +1,4 @@
+package chylex.hee.render.util
+import net.minecraft.client.renderer.GlStateManager
+
+typealias GL = GlStateManager
--- src/main/java/chylex/hee/system/util/MathExt.kt
+++ src/main/java/chylex/hee/system/util/MathExt.kt
@@ -9,4 +9,8 @@ fun Float.ceilToInt() = ceil(this).toInt()
 fun Double.floorToInt() = floor(this).toInt()
 fun Double.ceilToInt() = ceil(this).toInt()
 
+inline fun square(value: Int) = value * value
+inline fun square(value: Float) = value * value
+inline fun square(value: Double) = value * value
+
 infix fun Int.over(denominator: Int): Fraction = Fraction.getFraction(this, denominator)
