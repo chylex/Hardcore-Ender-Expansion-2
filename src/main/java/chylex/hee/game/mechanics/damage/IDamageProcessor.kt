406166b9f473d39bc8feb7a1f38089ffc0c725d3
chylex <contact@chylex.com>
2018-09-08 13:25:16 +0200

    Implement damage modifiers for armor, shields, and status effects

--- src/main/java/chylex/hee/game/mechanics/damage/IDamageProcessor.kt
+++ src/main/java/chylex/hee/game/mechanics/damage/IDamageProcessor.kt
@@ -1,10 +1,15 @@
 package chylex.hee.game.mechanics.damage
 import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.MobEffects
+import net.minecraft.item.ItemArmor
+import net.minecraft.potion.PotionEffect
 import net.minecraft.world.EnumDifficulty.EASY
 import net.minecraft.world.EnumDifficulty.HARD
 import net.minecraft.world.EnumDifficulty.NORMAL
 import net.minecraft.world.EnumDifficulty.PEACEFUL
+import kotlin.math.min
 import kotlin.math.nextUp
 
 interface IDamageProcessor{
@@ -52,6 +57,66 @@ interface IDamageProcessor{
 		val DEAL_CREATIVE = object: IDamageProcessor{
 			override fun setup(properties: DamageProperties.Writer){
 				properties.setDealCreative()
+			}
+		}
+		
+		// Equipment
+		
+		fun ARMOR_PROTECTION(allowShield: Boolean) = object: IDamageProcessor{
+			override fun setup(properties: DamageProperties.Writer){
+				if (allowShield){
+					properties.setAllowArmorAndShield()
+				}
+				else{
+					properties.setAllowArmor()
+				}
+			}
+		}
+		
+		val NUDITY_DANGER = object: IDamageProcessor{
+			override fun modifyDamage(amount: Float, target: Entity, properties: DamageProperties.Reader): Float{
+				val bodyCoverageFactor = target.armorInventoryList.sumBy {
+					if (it.item is ItemArmor)
+						2
+					else if (!it.isEmpty)
+						1
+					else
+						0
+				}
+				
+				return amount * when(bodyCoverageFactor){
+					0 -> 2.5F
+					1 -> 2.3F
+					2 -> 2.1F
+					3 -> 1.9F
+					4 -> 1.7F
+					5 -> 1.5F
+					6 -> 1.3F
+					7 -> 1.15F
+					else -> 1F
+				}
+			}
+		}
+		
+		// Status effects
+		
+		val POTION_PROTECTION = object: IDamageProcessor{
+			/**
+			 * [EntityLivingBase.applyPotionDamageCalculations]
+			 */
+			override fun modifyDamage(amount: Float, target: Entity, properties: DamageProperties.Reader): Float{
+				return if (target !is EntityLivingBase)
+					amount
+				else
+					amount * (1F - (0.15F * min(5, target.getActivePotionEffect(MobEffects.RESISTANCE)?.amplifier?.plus(1) ?: 0)))
+			}
+		}
+		
+		fun STATUS(effect: PotionEffect) = object: IDamageProcessor{
+			override fun afterDamage(target: Entity, properties: DamageProperties.Reader){
+				if (target is EntityLivingBase){
+					target.addPotionEffect(effect)
+				}
 			}
 		}
 	}
