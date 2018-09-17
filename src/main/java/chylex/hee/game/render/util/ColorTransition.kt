e64719a13cfbefaba10e2394a544b3c2e60490e0
chylex <contact@chylex.com>
2018-09-17 23:50:46 +0200

    Rewrite class for color transitions to improve queueing

---
+++ src/main/java/chylex/hee/game/render/util/ColorTransition.kt
@@ -0,0 +1,77 @@
+package chylex.hee.game.render.util
+import net.minecraft.client.Minecraft
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+
+@SideOnly(Side.CLIENT)
+class ColorTransition(private val defaultColor: HCL, private val transitionDuration: Float){
+	private var currentFrom: HCL = defaultColor
+	private var currentTo: HCL? = null
+	private var currentProgress = 0F
+	
+	private val transitionQueue = ArrayList<HCL>(4)
+	
+	private var lastUpdateTime = 0L
+	
+	val currentTargetColor
+		get() = currentTo ?: currentFrom
+	
+	val lastColorInQueue
+		get() = transitionQueue.lastOrNull()
+	
+	fun resetAll(){
+		resetQueue()
+		
+		currentFrom = defaultColor
+		currentTo = null
+		currentProgress = 0F
+		
+		lastUpdateTime = Minecraft.getSystemTime()
+	}
+	
+	fun resetQueue(){
+		transitionQueue.clear()
+	}
+	
+	fun enqueue(color: HCL){
+		if (currentTo == null){
+			currentTo = color
+		}
+		else{
+			transitionQueue.add(color)
+		}
+	}
+	
+	fun updateGetColor(): HCL{
+		val currentTime = Minecraft.getSystemTime()
+		val elapsedTime = currentTime - lastUpdateTime
+		
+		lastUpdateTime = currentTime
+		
+		val transitionTo = currentTo
+		
+		if (transitionTo == null){
+			return currentFrom
+		}
+		
+		currentProgress += elapsedTime / transitionDuration
+		
+		if (currentProgress >= 1F){
+			currentProgress = 0F
+			
+			currentFrom = transitionTo
+			currentTo = if (transitionQueue.isEmpty()) null else transitionQueue.removeAt(0)
+			
+			return currentFrom
+		}
+		else{
+			val hueSource = if (transitionTo.chroma > 0F && currentFrom.chroma == 0F) transitionTo else currentFrom
+			
+			return HCL(
+				hue = hueSource.hue,
+				chroma = currentFrom.chroma + (transitionTo.chroma - currentFrom.chroma) * currentProgress,
+				luminance = currentFrom.luminance + (transitionTo.luminance - currentFrom.luminance) * currentProgress
+			)
+		}
+	}
+}
