6bda95eddedabda2ea36f330a7e21e6fce63d2dc
chylex <contact@chylex.com>
2018-09-24 09:58:29 +0200

    Add AI to hide in or spawn from blocks

---
+++ src/main/java/chylex/hee/game/entity/living/ai/AIHideInBlock.kt
@@ -0,0 +1,33 @@
+package chylex.hee.game.entity.living.ai
+import chylex.hee.game.entity.living.ai.util.AIBaseContinuous
+import chylex.hee.system.util.Facing6
+import chylex.hee.system.util.Pos
+import chylex.hee.system.util.add
+import chylex.hee.system.util.getState
+import chylex.hee.system.util.posVec
+import chylex.hee.system.util.setState
+import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.EntityCreature
+
+class AIHideInBlock(
+	private val entity: EntityCreature,
+	private val chancePerTick: Int,
+	private val tryHideInBlock: (IBlockState) -> IBlockState?
+) : AIBaseContinuous(){
+	override fun tick(){
+		if (entity.attackTarget == null &&
+			entity.navigator.noPath() &&
+			entity.world.gameRules.getBoolean("mobGriefing") &&
+			entity.rng.nextInt(chancePerTick) == 0
+		){
+			val world = entity.world
+			val targetPos = Pos(entity.posVec.add(0.0, 0.5, 0.0)).offset(Facing6.randomOne(entity.rng))
+			
+			tryHideInBlock(targetPos.getState(world))?.let{
+				targetPos.setState(world, it)
+				entity.spawnExplosionParticle()
+				entity.setDead()
+			}
+		}
+	}
+}
---
+++ src/main/java/chylex/hee/game/entity/living/ai/AISummonFromBlock.kt
@@ -0,0 +1,63 @@
+package chylex.hee.game.entity.living.ai
+import chylex.hee.system.util.AIBase
+import chylex.hee.system.util.breakBlock
+import chylex.hee.system.util.getState
+import chylex.hee.system.util.nextInt
+import net.minecraft.block.Block
+import net.minecraft.entity.EntityCreature
+import net.minecraft.world.EnumDifficulty.PEACEFUL
+
+class AISummonFromBlock(
+	private val entity: EntityCreature,
+	private val searchAttempts: Int,
+	private val searchDistance: Int,
+	private val searchingFor: Block
+) : AIBase(){
+	private var summonInTicks = Int.MAX_VALUE
+	
+	fun triggerSummonInTicks(ticks: Int){
+		if (summonInTicks > ticks){
+			summonInTicks = ticks.coerceAtLeast(0)
+		}
+	}
+	
+	override fun shouldExecute(): Boolean{
+		return summonInTicks != Int.MAX_VALUE
+	}
+	
+	override fun updateTask(){
+		if (--summonInTicks > 0){
+			return
+		}
+		
+		summonInTicks = Int.MAX_VALUE
+		
+		val world = entity.world
+		val difficulty = world.difficulty
+		
+		if (difficulty == PEACEFUL || !world.gameRules.getBoolean("mobGriefing")){
+			return
+		}
+		
+		val rand = entity.rng
+		val pos = entity.position
+		
+		var remainingSpawns = 1 + rand.nextInt(0, difficulty.difficultyId)
+		
+		repeat(searchAttempts){
+			val checkedPos = pos.add(
+				rand.nextInt(-searchDistance, searchDistance),
+				rand.nextInt(-searchDistance / 2, searchDistance / 2),
+				rand.nextInt(-searchDistance, searchDistance)
+			)
+			
+			if (checkedPos.getState(world).block == searchingFor){
+				checkedPos.breakBlock(world, true)
+				
+				if (--remainingSpawns == 0){
+					return
+				}
+			}
+		}
+	}
+}
