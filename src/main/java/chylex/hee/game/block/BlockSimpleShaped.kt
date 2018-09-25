0aefcd227d232b5397af09810fc4e8afafc1778d
chylex <contact@chylex.com>
2018-09-25 12:23:59 +0200

    Fix block culling in BlockSimpleShaped

--- src/main/java/chylex/hee/game/block/BlockSimpleShaped.kt
+++ src/main/java/chylex/hee/game/block/BlockSimpleShaped.kt
@@ -6,4 +6,7 @@ import net.minecraft.world.IBlockAccess
 
 open class BlockSimpleShaped(builder: BlockSimple.Builder, private val aabb: AxisAlignedBB) : BlockSimple(builder){
 	override fun getBoundingBox(state: IBlockState, source: IBlockAccess, pos: BlockPos): AxisAlignedBB = aabb
+	
+	override fun isFullCube(state: IBlockState): Boolean = false
+	override fun isOpaqueCube(state: IBlockState): Boolean = false
 }
