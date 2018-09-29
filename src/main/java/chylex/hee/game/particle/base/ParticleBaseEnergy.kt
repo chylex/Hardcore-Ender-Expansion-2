b10a2ce8162bdcd5cf0f585b1c33a281edb972b9
chylex <contact@chylex.com>
2018-09-29 14:24:40 +0200

    Add a base class for Energy particles

---
+++ src/main/java/chylex/hee/game/particle/base/ParticleBaseEnergy.kt
@@ -0,0 +1,64 @@
+package chylex.hee.game.particle.base
+import chylex.hee.game.render.util.GL
+import chylex.hee.system.Resource
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.BufferBuilder
+import net.minecraft.client.renderer.GlStateManager.DestFactor.ONE
+import net.minecraft.client.renderer.GlStateManager.SourceFactor.SRC_ALPHA
+import net.minecraft.client.renderer.Tessellator
+import net.minecraft.client.renderer.texture.TextureAtlasSprite
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats
+import net.minecraft.entity.Entity
+import net.minecraft.world.World
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import org.lwjgl.opengl.GL11
+
+@SideOnly(Side.CLIENT)
+abstract class ParticleBaseEnergy(world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double) : ParticleBaseFloating(world, posX, posY, posZ, motX, motY, motZ){
+	private companion object{
+		@JvmStatic private val TEX_PARTICLE = Resource.Custom("textures/particle/energy.png")
+		
+		@SideOnly(Side.CLIENT)
+		private object TextureDescription : TextureAtlasSprite(TEX_PARTICLE.toString()){
+			override fun getMinU(): Float = 0F
+			override fun getMinV(): Float = 0F
+			override fun getMaxU(): Float = 1F
+			override fun getMaxV(): Float = 1F
+		}
+	}
+	
+	init{
+		particleTexture = TextureDescription
+	}
+	
+	override fun renderParticle(buffer: BufferBuilder, entity: Entity, partialTicks: Float, rotationX: Float, rotationZ: Float, rotationYZ: Float, rotationXY: Float, rotationXZ: Float){
+		val mc = Minecraft.getMinecraft()
+		
+		GL.enableBlend()
+		GL.blendFunc(SRC_ALPHA, ONE)
+		
+		GL.disableAlpha()
+		GL.disableLighting()
+		GL.depthMask(false)
+		
+		GL.color(1F, 1F, 1F, 1F)
+		
+		mc.entityRenderer.setupFogColor(true)
+		mc.renderEngine.bindTexture(TEX_PARTICLE)
+		
+		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP)
+		super.renderParticle(buffer, entity, partialTicks, rotationX, rotationZ, rotationYZ, rotationXY, rotationXZ)
+		Tessellator.getInstance().draw()
+		
+		mc.entityRenderer.setupFogColor(false)
+		
+		GL.depthMask(true)
+		GL.enableLighting()
+		GL.enableAlpha()
+		GL.disableBlend()
+	}
+	
+	final override fun getFXLayer(): Int = 3
+	final override fun shouldDisableDepth(): Boolean = true // doesn't do anything for getFXLayer == 3, but keep just in case
+}
