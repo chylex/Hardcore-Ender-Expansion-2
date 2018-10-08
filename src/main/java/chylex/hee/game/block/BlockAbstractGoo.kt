a549fe9b7bd7667310bf3a0474ef68f1f11bfd86
chylex <contact@chylex.com>
2018-10-09 01:50:34 +0200

    Implement Ender Goo affecting entity motion

--- src/main/java/chylex/hee/game/block/BlockAbstractGoo.kt
+++ src/main/java/chylex/hee/game/block/BlockAbstractGoo.kt
@@ -4,12 +4,15 @@ import chylex.hee.game.item.util.BlockEditor
 import chylex.hee.system.util.FLAG_NOTIFY_NEIGHBORS
 import chylex.hee.system.util.FLAG_RENDER_IMMEDIATE
 import chylex.hee.system.util.FLAG_SYNC_CLIENT
+import chylex.hee.system.util.Pos
+import chylex.hee.system.util.allInBoxMutable
 import chylex.hee.system.util.getState
 import chylex.hee.system.util.setBlock
 import net.minecraft.block.material.MapColor
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.Entity
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.init.Blocks
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
@@ -26,8 +29,11 @@ import net.minecraftforge.fml.common.eventhandler.Event.Result.ALLOW
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
+import java.util.UUID
 
 abstract class BlockAbstractGoo(private val fluid: FluidBase, material: Material) : BlockFluidClassic(fluid, material){
+	private var lastCollidingEntity = ThreadLocal<Pair<Long, UUID>?>()
+	
 	protected abstract val filledBucket: Item
 	
 	init{
@@ -72,6 +78,45 @@ abstract class BlockAbstractGoo(private val fluid: FluidBase, material: Material
 		e.filledBucket = ItemStack(filledBucket)
 		e.result = ALLOW
 	}
+	
+	override fun onEntityCollidedWithBlock(world: World, pos: BlockPos, state: IBlockState, entity: Entity){
+		if (!(entity is EntityPlayer && entity.capabilities.isFlying)){
+			
+			/*
+			 * this prevents calling modifyEntityMotion multiple times if the entity is touching 2 or more goo blocks
+			 *
+			 * because onEntityCollidedWithBlock is always called in succession for all blocks colliding with an entity,
+			 * it is enough to compare if either the world time or the entity has changed since last call (on the same thread)
+			 */
+			
+			val currentWorldTime = world.totalWorldTime
+			
+			if (lastCollidingEntity.get()?.takeUnless { it.first != currentWorldTime || it.second != entity.uniqueID } == null){
+				lastCollidingEntity.set(Pair(currentWorldTime, entity.uniqueID))
+				
+				// handling from Entity.doBlockCollisions
+				val bb = entity.entityBoundingBox
+				val posMin = Pos(bb.minX - 0.001, bb.minY - 0.001, bb.minZ - 0.001)
+				val posMax = Pos(bb.maxX + 0.001, bb.maxY + 0.001, bb.maxZ + 0.001)
+				
+				var lowestLevel = Int.MAX_VALUE
+				
+				for(testPos in posMin.allInBoxMutable(posMax)){
+					val level = testPos.getState(world).takeIf { it.block === this }?.getValue(LEVEL) ?: continue
+					
+					if (level < lowestLevel){
+						lowestLevel = level
+					}
+				}
+				
+				if (lowestLevel != Int.MAX_VALUE){
+					modifyEntityMotion(entity, lowestLevel)
+				}
+			}
+		}
+	}
+	
+	abstract fun modifyEntityMotion(entity: Entity, level: Int)
 	
 	// Colors
 	
--- src/main/java/chylex/hee/game/block/BlockEnderGoo.kt
+++ src/main/java/chylex/hee/game/block/BlockEnderGoo.kt
@@ -2,8 +2,20 @@ package chylex.hee.game.block
 import chylex.hee.game.block.fluid.FluidEnderGoo
 import chylex.hee.game.block.material.Materials
 import chylex.hee.init.ModItems
+import net.minecraft.entity.Entity
+import kotlin.math.pow
 
 open class BlockEnderGoo : BlockAbstractGoo(FluidEnderGoo, Materials.ENDER_GOO){
 	override val filledBucket
 		get() = ModItems.ENDER_GOO_BUCKET
+	
+	// Behavior
+	
+	override fun modifyEntityMotion(entity: Entity, level: Int){
+		val strength = ((quantaPerBlock - level) / quantaPerBlockFloat).pow(1.75F)
+		
+		entity.motionX *= 0.8 - (0.75 * strength)
+		entity.motionY *= 1.0 - (0.24 * strength)
+		entity.motionZ *= 0.8 - (0.75 * strength)
+	}
 }
