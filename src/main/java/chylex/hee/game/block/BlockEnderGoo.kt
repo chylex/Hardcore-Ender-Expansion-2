20c8db8efa85982e575583eda2532939626e51be
chylex <contact@chylex.com>
2018-10-10 13:16:30 +0200

    Add particle effects to Ender Goo

--- src/main/java/chylex/hee/game/block/BlockEnderGoo.kt
+++ src/main/java/chylex/hee/game/block/BlockEnderGoo.kt
@@ -5,11 +5,20 @@ import chylex.hee.game.block.fluid.FluidEnderGoo
 import chylex.hee.game.block.material.Materials
 import chylex.hee.game.entity.CustomCreatureType
 import chylex.hee.game.mechanics.potion.PotionLifeless.LIFELESS
+import chylex.hee.game.particle.ParticleEnderGoo
+import chylex.hee.game.particle.spawner.ParticleSpawnerCustom
+import chylex.hee.game.particle.util.IOffset.Constant
+import chylex.hee.game.particle.util.IOffset.InBox
+import chylex.hee.game.particle.util.IShape.Point
 import chylex.hee.init.ModItems
 import chylex.hee.system.Resource
+import chylex.hee.system.util.Pos
+import chylex.hee.system.util.add
 import chylex.hee.system.util.forge.capabilities.CapabilityProvider
 import chylex.hee.system.util.forge.capabilities.NullFactory
 import chylex.hee.system.util.forge.capabilities.NullStorage
+import chylex.hee.system.util.getBlock
+import chylex.hee.system.util.posVec
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
@@ -18,6 +27,8 @@ import net.minecraft.init.MobEffects.MINING_FATIGUE
 import net.minecraft.init.MobEffects.POISON
 import net.minecraft.init.MobEffects.WEAKNESS
 import net.minecraft.nbt.NBTTagInt
+import net.minecraft.util.EnumFacing.DOWN
+import net.minecraft.util.EnumFacing.UP
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.common.MinecraftForge
@@ -26,10 +37,30 @@ import net.minecraftforge.common.capabilities.CapabilityInject
 import net.minecraftforge.common.capabilities.CapabilityManager
 import net.minecraftforge.event.AttachCapabilitiesEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import java.util.Random
 import kotlin.math.pow
 
 open class BlockEnderGoo : BlockAbstractGoo(FluidEnderGoo, Materials.ENDER_GOO){
 	private companion object{
+		private val PARTICLE_STATIONARY = ParticleSpawnerCustom(
+			type = ParticleEnderGoo,
+			pos = Constant(0.25F, UP) + InBox(0.5F, 0.05F, 0.5F),
+			mot = Constant(0.11F, UP) + InBox(0.05F, 0.01F, 0.05F)
+		)
+		
+		private val PARTICLE_FLOWING = ParticleSpawnerCustom(
+			type = ParticleEnderGoo,
+			pos = Constant(0.1F, DOWN) + InBox(0.4F, 0.1F, 0.4F),
+			mot = Constant(0.08F, UP) + InBox(0.08F, 0.01F, 0.08F)
+		)
+		
+		private val PARTICLE_COLLISION = ParticleSpawnerCustom(
+			type = ParticleEnderGoo,
+			pos = Constant(0.9F, UP) + InBox(0.75F, 0F, 0.75F),
+			mot = Constant(0.11F, UP) + InBox(0.05F, 0.01F, 0.05F)
+		)
 		
 		// Collision tracking
 		
@@ -108,7 +139,21 @@ open class BlockEnderGoo : BlockAbstractGoo(FluidEnderGoo, Materials.ENDER_GOO){
 	// Behavior
 	
 	override fun modifyEntityMotion(entity: Entity, level: Int){
+		val world = entity.world
 		val strength = ((quantaPerBlock - level) / quantaPerBlockFloat).pow(1.75F)
+		
+		if (world.isRemote){
+			val rand = world.rand
+			
+			if (rand.nextFloat() < strength){
+				val motionMp = 12.5
+				val posVec = entity.posVec.add(entity.motionX * motionMp, 0.0, entity.motionZ * motionMp)
+				
+				if (Pos(posVec).getBlock(world) === this){
+					PARTICLE_COLLISION.spawn(Point(posVec, amount = 2), rand) // only triggered for the client player itself
+				}
+			}
+		}
 		
 		entity.motionX *= 0.8 - (0.75 * strength)
 		entity.motionY *= 1.0 - (0.24 * strength)
@@ -133,6 +178,20 @@ open class BlockEnderGoo : BlockAbstractGoo(FluidEnderGoo, Materials.ENDER_GOO){
 			if (!world.isRemote && entity.lifespan > 0){
 				--entity.lifespan
 			}
+		}
+	}
+	
+	// Client side
+	
+	@SideOnly(Side.CLIENT)
+	override fun randomDisplayTick(state: IBlockState, world: World, pos: BlockPos, rand: Random){
+		if (rand.nextBoolean()){
+			val particle = if (state.getValue(LEVEL) == 0)
+				PARTICLE_STATIONARY
+			else
+				PARTICLE_FLOWING
+			
+			particle.spawn(Point(pos, 1), rand)
 		}
 	}
 }
---
+++ src/main/java/chylex/hee/game/particle/ParticleEnderGoo.kt
@@ -0,0 +1,62 @@
+package chylex.hee.game.particle
+import chylex.hee.game.block.fluid.FluidEnderGoo
+import chylex.hee.game.particle.base.ParticleBaseFloating
+import chylex.hee.game.particle.spawner.factory.IParticleMaker
+import chylex.hee.system.util.nextFloat
+import chylex.hee.system.util.nextInt
+import chylex.hee.system.util.toRadians
+import net.minecraft.client.particle.Particle
+import net.minecraft.world.World
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import kotlin.math.PI
+
+object ParticleEnderGoo : IParticleMaker{
+	@SideOnly(Side.CLIENT)
+	override fun create(world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double, data: IntArray): Particle{
+		return Instance(world, posX, posY, posZ, motX, motY, motZ)
+	}
+	
+	private val COLOR = FluidEnderGoo.rgbColor.let { floatArrayOf(it.red / 255F, it.green / 255F, it.blue / 255F) }
+	
+	@SideOnly(Side.CLIENT)
+	private class Instance(world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double) : ParticleBaseFloating(world, posX, posY, posZ, motX, motY, motZ){
+		private var angleOffset = (rand.nextInt(5, 10) * (if (rand.nextBoolean()) 1F else -1F)).toRadians().toFloat()
+		
+		init{
+			particleTextureIndexX = rand.nextInt(8)
+			particleTextureIndexY = 0
+			
+			val colorMp = if (rand.nextInt(10) == 0)
+				rand.nextFloat(0.3F, 0.6F)
+			else
+				0.9F
+			
+			particleRed   = COLOR[0] * colorMp * rand.nextFloat(0.8F, 1F)
+			particleGreen = COLOR[1] * colorMp * rand.nextFloat(0.6F, 1F)
+			particleBlue  = COLOR[2] * colorMp * rand.nextFloat(0.9F, 1F)
+			
+			particleAngle = rand.nextFloat(0.0, PI).toFloat()
+			particleScale = rand.nextFloat(1.0F, 1.4F)
+			
+			particleGravity = 0.15F
+			
+			particleMaxAge = rand.nextInt(34, 42)
+		}
+		
+		override fun onUpdate(){
+			super.onUpdate()
+			
+			prevParticleAngle = particleAngle
+			particleAngle += angleOffset
+			angleOffset *= 0.94F
+			
+			if (particleAge > particleMaxAge - 15){
+				particleScale *= 0.9F
+			}
+			
+			motionX += rand.nextFloat(-0.02F, 0.02F)
+			motionZ += rand.nextFloat(-0.02F, 0.02F)
+		}
+	}
+}
