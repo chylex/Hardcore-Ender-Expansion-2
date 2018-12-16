b46c885f7c31d9b7d769d8be5e45336b11297689
chylex <contact@chylex.com>
2018-12-16 07:02:06 +0100

    Add base Gui+Container classes for custom inventory interfaces

---
+++ src/main/java/chylex/hee/game/gui/base/ContainerBaseCustomInventory.kt
@@ -0,0 +1,66 @@
+package chylex.hee.game.gui.base
+import chylex.hee.system.util.size
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.inventory.Container
+import net.minecraft.inventory.IInventory
+import net.minecraft.inventory.Slot
+import net.minecraft.item.ItemStack
+
+abstract class ContainerBaseCustomInventory<T : IInventory>(player: EntityPlayer, val containerInventory: T, ySize: Int) : Container(){
+	init{
+		containerInventory.openInventory(player)
+		setupSlots()
+		
+		val playerInventory = player.inventory
+		
+		for(row in 0 until 3){
+			for(col in 0 until 9){
+				addSlotToContainer(Slot(playerInventory, 9 + col + (row * 9), 8 + (col * 18), ySize - 82 + (row * 18)))
+			}
+		}
+		
+		for(col in 0 until 9){
+			addSlotToContainer(Slot(playerInventory, col, 8 + (col * 18), ySize - 24))
+		}
+	}
+	
+	protected abstract fun setupSlots()
+	
+	override fun canInteractWith(player: EntityPlayer): Boolean{
+		return containerInventory.isUsableByPlayer(player)
+	}
+	
+	override fun transferStackInSlot(player: EntityPlayer, index: Int): ItemStack{
+		val slot = inventorySlots[index]
+		
+		if (slot == null || !slot.hasStack){
+			return ItemStack.EMPTY
+		}
+		
+		val modifiableStack = slot.stack
+		val originalStack = modifiableStack.copy()
+		
+		if (index < containerInventory.size){
+			if (!mergeItemStack(modifiableStack, containerInventory.size, inventorySlots.size, true)){
+				return ItemStack.EMPTY
+			}
+		}
+		else if (!mergeItemStack(modifiableStack, 0, containerInventory.size, false)){
+			return ItemStack.EMPTY
+		}
+		
+		if (modifiableStack.isEmpty){
+			slot.putStack(ItemStack.EMPTY)
+		}
+		else{
+			slot.onSlotChanged()
+		}
+		
+		return originalStack
+	}
+	
+	override fun onContainerClosed(player: EntityPlayer){
+		super.onContainerClosed(player)
+		containerInventory.closeInventory(player)
+	}
+}
---
+++ src/main/java/chylex/hee/game/gui/base/GuiBaseCustomInventory.kt
@@ -0,0 +1,42 @@
+package chylex.hee.game.gui.base
+import chylex.hee.game.render.util.GL
+import chylex.hee.game.render.util.RGB
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.inventory.GuiContainer
+import net.minecraft.client.resources.I18n
+import net.minecraft.inventory.IInventory
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+
+@SideOnly(Side.CLIENT)
+abstract class GuiBaseCustomInventory<T : IInventory>(container: ContainerBaseCustomInventory<T>) : GuiContainer(container){
+	private companion object{
+		private val COLOR_TEXT = RGB(64u).toInt()
+	}
+	
+	protected abstract val texBackground: ResourceLocation
+	protected abstract val titleContainer: String
+	
+	private val titleInventory = Minecraft.getMinecraft().player.inventory.displayName.unformattedText // 'mc' not initialized yet
+	
+	override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float){
+		drawDefaultBackground()
+		super.drawScreen(mouseX, mouseY, partialTicks)
+		renderHoveredToolTip(mouseX, mouseY)
+	}
+	
+	override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int){
+		val x = (width - xSize) / 2
+		val y = (height - ySize) / 2
+		
+		GL.color(1F, 1F, 1F, 1F)
+		mc.textureManager.bindTexture(texBackground)
+		drawTexturedModalRect(x, y, 0, 0, xSize, ySize)
+	}
+	
+	override fun drawGuiContainerForegroundLayer(mouseX: Int, mouseY: Int){
+		fontRenderer.drawString(I18n.format(titleContainer), 8, 6, COLOR_TEXT)
+		fontRenderer.drawString(titleInventory, 8, ySize - 94, COLOR_TEXT)
+	}
+}
