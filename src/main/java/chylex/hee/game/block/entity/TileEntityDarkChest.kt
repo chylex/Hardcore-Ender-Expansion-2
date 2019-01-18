f9d4542c9000348bac847541a7988035308af0d6
chylex <contact@chylex.com>
2019-01-18 01:17:41 +0100

    Fix double Dark Chest render bounding box

--- src/main/java/chylex/hee/game/block/entity/TileEntityDarkChest.kt
+++ src/main/java/chylex/hee/game/block/entity/TileEntityDarkChest.kt
@@ -1,5 +1,13 @@
 package chylex.hee.game.block.entity
 import chylex.hee.game.block.BlockDarkChest
 import net.minecraft.tileentity.TileEntityChest
+import net.minecraft.util.math.AxisAlignedBB
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
 
-class TileEntityDarkChest : TileEntityChest(BlockDarkChest.TYPE)
+class TileEntityDarkChest : TileEntityChest(BlockDarkChest.TYPE){
+	@SideOnly(Side.CLIENT)
+	override fun getRenderBoundingBox(): AxisAlignedBB{
+		return AxisAlignedBB(pos.add(-1, 0, -1), pos.add(2, 2, 2))
+	}
+}
