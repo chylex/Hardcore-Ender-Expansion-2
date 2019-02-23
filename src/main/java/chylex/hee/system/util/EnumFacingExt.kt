c2def2fc6eaaee3bfb24a9e004a2dd2ff7a555a2
chylex <contact@chylex.com>
2019-02-23 14:24:09 +0100

    Add Rotation4 w/ permutation picker to EnumFacingExt

--- src/main/java/chylex/hee/system/util/EnumFacingExt.kt
+++ src/main/java/chylex/hee/system/util/EnumFacingExt.kt
@@ -1,6 +1,7 @@
 package chylex.hee.system.util
 import com.google.common.collect.Collections2
 import net.minecraft.util.EnumFacing
+import net.minecraft.util.Rotation
 import java.util.Random
 
 object Facing4 : List<EnumFacing> by EnumFacing.HORIZONTALS.toList(){
@@ -9,3 +10,8 @@ object Facing4 : List<EnumFacing> by EnumFacing.HORIZONTALS.toList(){
 }
 
 object Facing6 : List<EnumFacing> by EnumFacing.VALUES.toList()
+
+object Rotation4 : List<Rotation> by Rotation.values().toList(){
+	private val allPermutations = Collections2.permutations(this).toTypedArray()
+	fun randomPermutation(rand: Random) = rand.nextItem(allPermutations)
+}
