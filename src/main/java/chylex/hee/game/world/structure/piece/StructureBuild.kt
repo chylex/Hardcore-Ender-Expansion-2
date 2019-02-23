3f08d6d01178d95a6dda3b6d68431317135354e2
chylex <contact@chylex.com>
2019-02-23 15:51:37 +0100

    Work on structure piece code (merging, chaining & removal, mutability)

--- src/main/java/chylex/hee/game/world/structure/piece/StructureBuild.kt
+++ src/main/java/chylex/hee/game/world/structure/piece/StructureBuild.kt
@@ -1,43 +1,99 @@
 package chylex.hee.game.world.structure.piece
 import chylex.hee.game.world.structure.IStructureGenerator
 import chylex.hee.game.world.structure.IStructureWorld
+import chylex.hee.game.world.structure.piece.StructureBuild.AddMode.APPEND
+import chylex.hee.game.world.structure.piece.StructureBuild.AddMode.MERGE
 import chylex.hee.game.world.structure.piece.StructurePiece.Instance
+import chylex.hee.game.world.structure.piece.StructurePiece.MutableInstance
 import chylex.hee.game.world.structure.world.OffsetStructureWorld
 import chylex.hee.game.world.util.Size
 import net.minecraft.util.math.BlockPos
 
-class StructureBuild(val size: Size, startingPiece: Instance){
-	class PositionedPiece(val instance: Instance, val offset: BlockPos){
+class StructureBuild<T : MutableInstance>(val size: Size, startingPiece: T){
+	enum class AddMode{
+		APPEND, MERGE
+	}
+	
+	class PositionedPiece<T : Instance>(val instance: T, val offset: BlockPos){
 		val pieceBox = instance.size.toBoundingBox(offset)
+		
+		fun freeze(): PositionedPiece<Instance>{
+			return PositionedPiece(instance.freeze(), offset)
+		}
 	}
 	
 	private val structureBox = size.toBoundingBox(BlockPos.ORIGIN)
 	private val pieces = mutableListOf(PositionedPiece(startingPiece, size.centerPos.subtract(startingPiece.size.centerPos)))
+	private var chain = -1
 	
-	val generatedPieces: List<PositionedPiece> = pieces
+	val generatedPieces: List<PositionedPiece<T>> = pieces
 	
-	fun addPiece(newPiece: Instance, newPieceConnection: IStructurePieceConnection, targetPiece: PositionedPiece, targetPieceConnection: IStructurePieceConnection): Boolean{
+	fun addPiece(newPiece: T, newPieceConnection: IStructurePieceConnection, targetPiece: PositionedPiece<T>, targetPieceConnection: IStructurePieceConnection, mode: AddMode = APPEND): PositionedPiece<T>?{
 		val alignedPos = targetPiece.offset
 			.add(targetPieceConnection.offset)
-			.offset(targetPieceConnection.facing)
+			.offset(targetPieceConnection.facing, if (mode == MERGE) 0 else 1)
 			.subtract(newPieceConnection.offset)
 		
 		val addedPiece = PositionedPiece(newPiece, alignedPos)
 		val addedBox = addedPiece.pieceBox
 		
-		if (addedBox.isInside(structureBox) && pieces.none { addedBox.intersects(it.pieceBox) }){
-			newPiece.useConnection(newPieceConnection)
-			targetPiece.instance.useConnection(targetPieceConnection)
+		if (addedBox.isInside(structureBox) && pieces.none { addedBox.intersects(it.pieceBox) && (mode == APPEND || it !== targetPiece) }){
+			val targetInstance = targetPiece.instance
+			
+			newPiece.useConnection(newPieceConnection, targetInstance)
+			targetInstance.useConnection(targetPieceConnection, newPiece)
 			
 			pieces.add(addedPiece)
-			return true
+			
+			if (chain != -1){
+				++chain
+			}
+			
+			return addedPiece
 		}
 		
-		return false
+		return null
+	}
+	
+	fun beginChain(){
+		if (chain != -1){
+			throw IllegalStateException("cannot begin a structure build chain when the last one has not finished")
+		}
+		
+		chain = 0
+	}
+	
+	fun commitChain(){
+		chain = -1
+	}
+	
+	fun revertChain(){
+		while(--chain >= 0){ // ends at -1
+			val removed = pieces.removeAt(pieces.lastIndex).instance
+			
+			for(neighbor in removed.restoreAllConnections()){
+				if (!neighbor.restoreConnection(removed)){
+					throw IllegalStateException("failed reverting chain, found an asymmetric connection")
+				}
+			}
+		}
+	}
+	
+	inline fun guardChain(func: () -> Boolean): Boolean{
+		beginChain()
+		
+		return if (func()){
+			commitChain()
+			true
+		}
+		else{
+			revertChain()
+			false
+		}
 	}
 	
 	fun freeze(): IStructureGenerator{
-		val finalPieces = pieces.toTypedArray()
+		val finalPieces = pieces.map(PositionedPiece<*>::freeze).toTypedArray()
 		
 		return object : IStructureGenerator{
 			override val size = this@StructureBuild.size
--- src/main/java/chylex/hee/game/world/structure/piece/StructurePiece.kt
+++ src/main/java/chylex/hee/game/world/structure/piece/StructurePiece.kt
@@ -5,14 +5,13 @@ import chylex.hee.game.world.structure.world.RotatedStructureWorld
 import net.minecraft.block.BlockColored
 import net.minecraft.init.Blocks
 import net.minecraft.item.EnumDyeColor
-import net.minecraft.util.EnumFacing
 import net.minecraft.util.Rotation
 
 abstract class StructurePiece : IStructureGenerator{
 	protected abstract val connections: Array<IStructurePieceConnection>
 	
 	final override fun generate(world: IStructureWorld){
-		generate(world, Instance(Rotation.NONE).apply { connections.forEach(::useConnection) })
+		generate(world, MutableInstance(Rotation.NONE).apply { connections.forEach { useConnection(it, MutableInstance(Rotation.NONE)) } })
 		
 		for(connection in connections){
 			world.setState(connection.offset, Blocks.WOOL.defaultState.withProperty(BlockColored.COLOR, EnumDyeColor.values()[connection.facing.index - 2]))
@@ -21,42 +20,87 @@ abstract class StructurePiece : IStructureGenerator{
 	
 	protected abstract fun generate(world: IStructureWorld, instance: Instance)
 	
-	inner class Instance(private val rotation: Rotation) : IStructureGenerator{
-		override val size = this@StructurePiece.size.rotate(rotation)
+	// Rotated connection
+	
+	private inner class RotatedStructurePieceConnection(private val original: IStructurePieceConnection, rotation: Rotation) : IStructurePieceConnection{
+		override val offset = RotatedStructureWorld.rotatePos(original.offset, this@StructurePiece.size, rotation)
+		override val facing = rotation.rotate(original.facing)!!
 		
-		private inner class RotatedStructurePieceConnection(val original: IStructurePieceConnection) : IStructurePieceConnection{
-			override val offset = RotatedStructureWorld.rotatePos(original.offset, this@StructurePiece.size, rotation)
-			override val facing = rotation.rotate(original.facing)!!
-			
-			override fun canConnectWith(other: IStructurePieceConnection): Boolean{
-				return original.canConnectWith((other as? RotatedStructurePieceConnection)?.original ?: other)
-			}
-			
-			fun matches(other: IStructurePieceConnection): Boolean{
-				return this === other || original === other
-			}
+		override fun canConnectWith(other: IStructurePieceConnection): Boolean{
+			return original.canConnectWith((other as? RotatedStructurePieceConnection)?.original ?: other)
 		}
 		
-		private val availableConnections = this@StructurePiece.connections.map(::RotatedStructurePieceConnection).toMutableList()
+		fun matches(other: IStructurePieceConnection): Boolean{
+			return this === other || original === other
+		}
+	}
+	
+	// Frozen instance
+	
+	open inner class Instance private constructor(private val rotation: Rotation, private val availableConnections: List<RotatedStructurePieceConnection>) : IStructureGenerator{
+		constructor(rotation: Rotation) : this(rotation, this@StructurePiece.connections.map { RotatedStructurePieceConnection(it, rotation) }.toMutableList())
 		
-		fun findValidConnections(facing: EnumFacing): List<IStructurePieceConnection>{
-			return availableConnections.filter { it.facing == facing }
+		final override val size = this@StructurePiece.size.rotate(rotation)
+		
+		val hasAvailableConnections
+			get() = availableConnections.isNotEmpty()
+		
+		fun findValidConnections(): List<IStructurePieceConnection>{
+			return availableConnections
 		}
 		
-		fun findValidConnections(facing: EnumFacing, with: IStructurePieceConnection): List<IStructurePieceConnection>{
-			return availableConnections.filter { it.facing == facing && it.canConnectWith(with) }
+		fun findValidConnections(with: IStructurePieceConnection): List<IStructurePieceConnection>{
+			val targetFacing = with.facing.opposite
+			return availableConnections.filter { it.facing == targetFacing && it.canConnectWith(with) }
 		}
 		
 		fun isConnectionUsed(connection: IStructurePieceConnection): Boolean{
 			return availableConnections.none { it.matches(connection) }
 		}
 		
-		fun useConnection(connection: IStructurePieceConnection){
-			availableConnections.removeIf { it.matches(connection) }
+		fun freeze(): Instance{
+			return Instance(rotation, availableConnections.toList())
 		}
 		
-		override fun generate(world: IStructureWorld){
+		final override fun generate(world: IStructureWorld){
 			this@StructurePiece.generate(RotatedStructureWorld(world, this@StructurePiece.size, rotation), this)
+		}
+	}
+	
+	// Mutable instance
+	
+	open inner class MutableInstance(rotation: Rotation) : Instance(rotation){
+		private val availableConnections = findValidConnections() as MutableList<RotatedStructurePieceConnection> // ugly implementation detail
+		private val usedConnections = mutableMapOf<RotatedStructurePieceConnection, MutableInstance>()
+		
+		fun useConnection(connection: IStructurePieceConnection, neighbor: MutableInstance){
+			val index = availableConnections.indexOfFirst { it.matches(connection) }
+			
+			if (index != -1){
+				availableConnections.removeAt(index).let { usedConnections[it] = neighbor }
+			}
+		}
+		
+		fun restoreConnection(neighbor: MutableInstance): Boolean{
+			val used = usedConnections.filterValues { it === neighbor }.keys
+			
+			if (used.isEmpty()){
+				return false
+			}
+			
+			for(connection in used){
+				availableConnections.add(connection)
+				usedConnections.remove(connection)
+			}
+			
+			return true
+		}
+		
+		fun restoreAllConnections(): List<MutableInstance>{
+			return usedConnections.values.toList().also {
+				availableConnections.addAll(usedConnections.keys)
+				usedConnections.clear()
+			}
 		}
 	}
 }
