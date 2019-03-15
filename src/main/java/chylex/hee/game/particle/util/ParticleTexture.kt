ba97cf077ed7ba0801d910a19c958a574032ffb1
chylex <contact@chylex.com>
2019-03-15 13:14:32 +0100

    Add pixel particle texture

--- src/main/java/chylex/hee/game/particle/util/ParticleTexture.kt
+++ src/main/java/chylex/hee/game/particle/util/ParticleTexture.kt
@@ -9,14 +9,17 @@ import net.minecraftforge.fml.relauncher.Side
 
 @EventBusSubscriber(Side.CLIENT, modid = HEE.ID)
 object ParticleTexture{
+	lateinit var PIXEL: TextureAtlasSprite private set
 	lateinit var STAR: TextureAtlasSprite private set
 	
+	private val TEX_PIXEL = Resource.Custom("particle/pixel")
 	private val TEX_STAR = Resource.Custom("particle/star")
 	
 	@JvmStatic
 	@SubscribeEvent
 	fun onTextureStitchPre(e: TextureStitchEvent.Pre){
 		with(e.map){
+			registerSprite(TEX_PIXEL)
 			registerSprite(TEX_STAR)
 		}
 	}
@@ -25,6 +28,7 @@ object ParticleTexture{
 	@SubscribeEvent
 	fun onTextureStitchPost(e: TextureStitchEvent.Post){
 		with(e.map){
+			PIXEL = getAtlasSprite(TEX_PIXEL.toString())
 			STAR = getAtlasSprite(TEX_STAR.toString())
 		}
 	}
