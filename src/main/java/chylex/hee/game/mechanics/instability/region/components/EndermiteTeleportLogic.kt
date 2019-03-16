713d22105d35f99f6d4cb5cde5a88916446b192a
chylex <contact@chylex.com>
2019-03-16 09:53:16 +0100

    Update EndermiteTeleportLogic to use Teleporter.nearLocation

--- src/main/java/chylex/hee/game/mechanics/instability/region/components/EndermiteTeleportLogic.kt
+++ src/main/java/chylex/hee/game/mechanics/instability/region/components/EndermiteTeleportLogic.kt
@@ -1,14 +1,8 @@
 package chylex.hee.game.mechanics.instability.region.components
 import chylex.hee.game.entity.living.EntityMobEndermiteInstability
 import chylex.hee.game.world.util.Teleporter
-import chylex.hee.system.util.Pos
-import chylex.hee.system.util.blocksMovement
-import chylex.hee.system.util.floorToInt
-import chylex.hee.system.util.nextFloat
-import chylex.hee.system.util.nextVector
 import chylex.hee.system.util.posVec
 import chylex.hee.system.util.removeItem
-import net.minecraft.util.math.Vec3d
 import java.util.Random
 import kotlin.math.min
 
@@ -28,18 +22,6 @@ internal object EndermiteTeleportLogic{
 	}
 	
 	private fun performTeleportation(endermite: EntityMobEndermiteInstability, rand: Random){
-		val world = endermite.world
-		val originalPos = endermite.posVec
-		val originalBox = endermite.entityBoundingBox
-		
-		repeat(64){
-			val randomPos = originalPos.add(rand.nextVector(rand.nextFloat(3.0, 6.0)))
-			val newPos = Vec3d(randomPos.x, randomPos.y.floorToInt() + 0.01, randomPos.z)
-			
-			if (Pos(newPos).down().blocksMovement(world) && world.getCollisionBoxes(endermite, originalBox.offset(newPos.subtract(originalPos))).isEmpty()){
-				TELEPORT.toLocation(endermite, newPos)
-				return
-			}
-		}
+		TELEPORT.nearLocation(endermite, rand, endermite.posVec, distance = (3.0)..(6.0), attempts = 64)
 	}
 }
