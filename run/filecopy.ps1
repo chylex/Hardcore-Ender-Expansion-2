a4eb927ef77a48f47d5f622affa0c1b388a10212
chylex <contact@chylex.com>
2019-03-28 21:29:35 +0100

    Fix filecopy.ps1 to work on older versions of PowerShell

--- run/filecopy.ps1
+++ run/filecopy.ps1
@@ -1,2 +1,5 @@
 Param([Parameter(Mandatory = $True, Position = 1)][string] $filePath)
-Set-Clipboard -Path $filePath
+Add-Type -AssemblyName System.Windows.Forms
+$files = new-object System.Collections.Specialized.StringCollection
+$files.Add($filePath)
+[System.Windows.Forms.Clipboard]::SetFileDropList($files)
