d48dff9fc8440ee3992762d3a63746bfd143dc1b
chylex <contact@chylex.com>
2019-03-31 09:39:52 +0200

    Refactor some structure utilities into an abstract class in IStructureBuilder

--- src/main/java/chylex/hee/game/world/structure/piece/IStructureBuilder.kt
+++ src/main/java/chylex/hee/game/world/structure/piece/IStructureBuilder.kt
@@ -1,7 +1,33 @@
 package chylex.hee.game.world.structure.piece
 import chylex.hee.game.world.structure.IStructureGenerator
+import chylex.hee.game.world.structure.piece.StructureBuild.AddMode
+import chylex.hee.game.world.structure.piece.StructureBuild.PositionedPiece
+import chylex.hee.game.world.structure.piece.StructurePiece.MutableInstance
+import chylex.hee.system.util.Rotation4
+import chylex.hee.system.util.nextItem
+import chylex.hee.system.util.nextItemOrNull
+import net.minecraft.util.Rotation
 import java.util.Random
 
 interface IStructureBuilder{
 	fun build(rand: Random): IStructureGenerator?
+	
+	abstract class ProcessBase<T : MutableInstance>(protected val build: StructureBuild<T>, protected val rand: Random){
+		protected fun baseAddPiece(mode: AddMode, targetPiece: PositionedPiece<T>, targetConnection: IStructurePieceConnection, generatedPieceConstructor: (Rotation) -> T): PositionedPiece<T>?{
+			for(rotation in Rotation4.randomPermutation(rand)){
+				val generatedInstance = generatedPieceConstructor(rotation)
+				val connections = generatedInstance.findAvailableConnections(targetConnection)
+				
+				if (connections.isNotEmpty()){
+					return build.addPiece(generatedInstance, rand.nextItem(connections), targetPiece, targetConnection, mode)
+				}
+			}
+			
+			return null
+		}
+		
+		protected fun baseAddPiece(mode: AddMode, targetPiece: PositionedPiece<T>, generatedPieceConstructor: (Rotation) -> T): PositionedPiece<T>?{
+			return rand.nextItemOrNull(targetPiece.instance.findAvailableConnections())?.let { targetConnection -> baseAddPiece(mode, targetPiece, targetConnection, generatedPieceConstructor) }
+		}
+	}
 }
