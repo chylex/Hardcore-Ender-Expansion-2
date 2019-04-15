d57b9d21b18c8722ba5ffb5b4e3c9160f322f53c
chylex <contact@chylex.com>
2019-04-15 02:43:09 +0200

    Add World.perSavefileData extension

--- src/main/java/chylex/hee/system/util/WorldExt.kt
+++ src/main/java/chylex/hee/system/util/WorldExt.kt
@@ -1,9 +1,20 @@
 package chylex.hee.system.util
 import net.minecraft.world.World
 import net.minecraft.world.storage.WorldSavedData
+import net.minecraftforge.fml.common.FMLCommonHandler
 
 inline fun <reified T : WorldSavedData> World.perDimensionData(name: String, constructor: (String) -> T): T{
 	return this.perWorldStorage.let {
 		it.getOrLoadData(T::class.java, name) as? T ?: constructor(name).also { data -> it.setData(name, data) }
 	}
+}
+
+inline fun <reified T : WorldSavedData> World.perSavefileData(name: String, constructor: (String) -> T): T{
+	return this.mapStorage!!.let {
+		it.getOrLoadData(T::class.java, name) as? T ?: constructor(name).also { data -> it.setData(name, data) }
+	}
+}
+
+inline fun <reified T : WorldSavedData> perSavefileData(name: String, constructor: (String) -> T): T{
+	return FMLCommonHandler.instance().minecraftServerInstance.entityWorld.perSavefileData(name, constructor)
 }
