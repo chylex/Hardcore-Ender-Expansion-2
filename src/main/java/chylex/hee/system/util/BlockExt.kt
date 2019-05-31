6cf9966804d854655847fa2de690bf29aa143a5e
chylex <contact@chylex.com>
2019-06-01 01:27:46 +0200

    Add Block & IBlockState extensions

---
+++ src/main/java/chylex/hee/system/util/BlockExt.kt
@@ -0,0 +1,26 @@
+package chylex.hee.system.util
+import net.minecraft.block.Block
+import net.minecraft.block.properties.IProperty
+import net.minecraft.block.properties.PropertyBool
+import net.minecraft.block.properties.PropertyInteger
+import net.minecraft.block.state.IBlockState
+
+inline fun <T : Comparable<T>, V : T> IBlockState.with(property: IProperty<T>, value: V): IBlockState{
+	return this.withProperty(property, value)
+}
+
+inline fun <T : Comparable<T>, V : T> Block.with(property: IProperty<T>, value: V): IBlockState{
+	return this.defaultState.withProperty(property, value)
+}
+
+inline operator fun <T : Comparable<T>> IBlockState.get(property: IProperty<T>): T{
+	return this.getValue(property)
+}
+
+inline operator fun IBlockState.get(property: PropertyBool): Boolean{
+	return this.getValue(property)
+}
+
+inline operator fun IBlockState.get(property: PropertyInteger): Int{
+	return this.getValue(property)
+}
