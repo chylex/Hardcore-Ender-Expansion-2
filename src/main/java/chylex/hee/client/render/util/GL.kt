756024f1cf992bdbf86db78e87510a08bcecda85
chylex <contact@chylex.com>
2019-06-03 02:02:42 +0200

    Add Tessellator extensions

--- src/main/java/chylex/hee/client/render/util/GL.kt
+++ src/main/java/chylex/hee/client/render/util/GL.kt
@@ -1,4 +1,15 @@
 package chylex.hee.client.render.util
+import net.minecraft.client.renderer.BufferBuilder
 import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.client.renderer.Tessellator
+import net.minecraft.client.renderer.vertex.VertexFormat
 
 typealias GL = GlStateManager
+
+val TESSELLATOR: Tessellator = Tessellator.getInstance()
+
+inline fun Tessellator.draw(mode: Int, format: VertexFormat, procedure: BufferBuilder.() -> Unit){
+	this.buffer.begin(mode, format)
+	this.buffer.apply(procedure)
+	this.draw()
+}
