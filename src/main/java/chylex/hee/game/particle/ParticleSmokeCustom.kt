491e2d99353b2fcd5565d3d0141cfc5d64d1c780
chylex <contact@chylex.com>
2019-06-23 16:15:34 +0200

    Allow custom smoke particle color

--- src/main/java/chylex/hee/game/particle/ParticleSmokeCustom.kt
+++ src/main/java/chylex/hee/game/particle/ParticleSmokeCustom.kt
@@ -1,6 +1,8 @@
 package chylex.hee.game.particle
 import chylex.hee.game.particle.spawner.factory.IParticleData
 import chylex.hee.game.particle.spawner.factory.IParticleMaker
+import chylex.hee.system.util.color.IColor
+import chylex.hee.system.util.color.RGB
 import chylex.hee.system.util.floorToInt
 import net.minecraft.client.particle.Particle
 import net.minecraft.client.particle.ParticleSmokeNormal
@@ -15,9 +17,11 @@ object ParticleSmokeCustom : IParticleMaker{
 	}
 	
 	class Data(
-		scale: Float = 1F
+		scale: Float = 1F,
+		color: IColor = RGB(255u)
 	) : IParticleData.Static(intArrayOf(
-		(scale * 100F).floorToInt()
+		(scale * 100F).floorToInt(),
+		color.toInt()
 	))
 	
 	private val DEFAULT_DATA = Data()
@@ -27,5 +31,13 @@ object ParticleSmokeCustom : IParticleMaker{
 		world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double, unsafeData: IntArray
 	) : ParticleSmokeNormal(
 		world, posX, posY, posZ, motX, motY, motZ, DEFAULT_DATA.validate(unsafeData)[0] * 0.01F
-	)
+	){
+		init{
+			val color = DEFAULT_DATA.validate(unsafeData)[1]
+			
+			particleRed = ((color shr 16) and 255) / 255F
+			particleGreen = ((color shr 8) and 255) / 255F
+			particleBlue = (color and 255) / 255F
+		}
+	}
 }
