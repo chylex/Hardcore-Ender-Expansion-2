78e0010990f4d4e30d6bcbe0ad7544d1d9db8f22
chylex <contact@chylex.com>
2019-07-18 15:47:24 +0200

    Refactor entity structure triggers to allow nudging by fractional x/z amounts

--- src/main/java/chylex/hee/game/world/structure/trigger/EntityStructureTrigger.kt
+++ src/main/java/chylex/hee/game/world/structure/trigger/EntityStructureTrigger.kt
@@ -2,23 +2,51 @@ package chylex.hee.game.world.structure.trigger
 import chylex.hee.game.entity.technical.EntityTechnicalTrigger
 import chylex.hee.game.world.structure.IStructureTrigger
 import chylex.hee.game.world.util.Transform
+import chylex.hee.system.util.component1
+import chylex.hee.system.util.component2
+import chylex.hee.system.util.component3
 import net.minecraft.entity.Entity
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 
-class EntityStructureTrigger(private val entityConstructor: (World) -> Entity, private val yOffset: Double) : IStructureTrigger{
+class EntityStructureTrigger private constructor(private val entityConstructor: (World) -> Entity, private val entityLocator: (BlockPos, Transform) -> Vec3d) : IStructureTrigger{
+	constructor(
+		entityConstructor: (World) -> Entity,
+		yOffset: Double
+	) : this(
+		entityConstructor,
+		{ pos, _ -> Vec3d(pos.x + 0.5, pos.y + yOffset, pos.z + 0.5) }
+	)
+	
+	constructor(
+		entityConstructor: (World) -> Entity,
+		nudgeFacing: EnumFacing,
+		nudgeAmount: Double,
+		yOffset: Double
+	) : this(
+		entityConstructor,
+		{ pos, transform -> transform(nudgeFacing).let { Vec3d(pos.x + 0.5 + (nudgeAmount * it.xOffset), pos.y + yOffset, pos.z + 0.5 + (nudgeAmount * it.zOffset)) } }
+	){
+		if (nudgeFacing.yOffset != 0){
+			throw IllegalArgumentException("entity trigger can only be nudged on x/z axis")
+		}
+	}
+	
 	constructor(
 		triggerType: EntityTechnicalTrigger.Types,
 		facing: EnumFacing = EnumFacing.SOUTH
 	) : this(
 		{ world -> EntityTechnicalTrigger(world, triggerType).apply { rotationYaw = facing.horizontalAngle } },
-		0.0
+		yOffset = 0.0
 	)
 	
 	override fun realize(world: World, pos: BlockPos, transform: Transform){
+		val (x, y, z) = entityLocator(pos, transform)
+		
 		entityConstructor(world).apply {
-			setLocationAndAngles(pos.x + 0.5, pos.y + this@EntityStructureTrigger.yOffset, pos.z + 0.5, rotationYaw, rotationPitch)
+			setLocationAndAngles(x, y, z, rotationYaw, rotationPitch)
 			transform(this)
 			world.spawnEntity(this)
 		}
