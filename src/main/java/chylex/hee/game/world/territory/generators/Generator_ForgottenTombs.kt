26c169c02e307008a04fdc495c1e31d80169ebdc
chylex <contact@chylex.com>
2019-07-19 12:16:41 +0200

    Implement Tomb Dungeon entrance generation in Forgotten Tombs

--- src/main/java/chylex/hee/game/world/territory/generators/Generator_ForgottenTombs.kt
+++ src/main/java/chylex/hee/game/world/territory/generators/Generator_ForgottenTombs.kt
@@ -1,17 +1,25 @@
 package chylex.hee.game.world.territory.generators
+import chylex.hee.HEE
 import chylex.hee.game.world.feature.basic.NoiseGenerator
 import chylex.hee.game.world.feature.basic.PortalGenerator
 import chylex.hee.game.world.feature.basic.ores.OreGenerator
 import chylex.hee.game.world.feature.basic.ores.impl.OreTechniqueAdjacent
 import chylex.hee.game.world.feature.basic.ores.impl.withAdjacentAirCheck
+import chylex.hee.game.world.feature.tombdungeon.TombDungeonBuilder
+import chylex.hee.game.world.feature.tombdungeon.TombDungeonPieces
+import chylex.hee.game.world.feature.tombdungeon.piece.TombDungeonStart
 import chylex.hee.game.world.generation.IBlockPlacer.BlockReplacer
 import chylex.hee.game.world.generation.SegmentedWorld
 import chylex.hee.game.world.generation.TerritoryGenerationInfo
+import chylex.hee.game.world.structure.IBlockPicker.Single
+import chylex.hee.game.world.structure.IBlockPicker.Single.Air
+import chylex.hee.game.world.structure.world.OffsetStructureWorld
 import chylex.hee.game.world.territory.ITerritoryGenerator
 import chylex.hee.game.world.util.BoundingBox
 import chylex.hee.game.world.util.PosXZ
 import chylex.hee.game.world.util.Size
 import chylex.hee.init.ModBlocks
+import chylex.hee.system.util.Facing4
 import chylex.hee.system.util.Facing6
 import chylex.hee.system.util.Pos
 import chylex.hee.system.util.allInBoxMutable
@@ -23,17 +31,25 @@ import chylex.hee.system.util.floorToInt
 import chylex.hee.system.util.nextBiasedFloat
 import chylex.hee.system.util.nextFloat
 import chylex.hee.system.util.nextInt
+import chylex.hee.system.util.nextItem
 import chylex.hee.system.util.offsetUntil
 import chylex.hee.system.util.remapRange
 import chylex.hee.system.util.square
+import chylex.hee.system.util.withFacing
+import chylex.hee.system.util.xz
 import net.minecraft.block.Block
 import net.minecraft.init.Blocks
 import net.minecraft.util.EnumFacing.DOWN
+import net.minecraft.util.EnumFacing.NORTH
+import net.minecraft.util.EnumFacing.SOUTH
 import net.minecraft.util.EnumFacing.UP
 import net.minecraft.util.math.BlockPos
 import java.util.Random
+import kotlin.math.PI
 import kotlin.math.abs
+import kotlin.math.cos
 import kotlin.math.pow
+import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 object Generator_ForgottenTombs : ITerritoryGenerator{
@@ -49,14 +65,16 @@ object Generator_ForgottenTombs : ITerritoryGenerator{
 		val spawnPoint = EntranceCave.generate(world, rand, size)
 		PortalGenerator.VoidPortalReturnActive.place(world, spawnPoint)
 		
-		// TODO tomb dungeon
+		val entrancePoint = TombDungeonEntrance.generate(world, rand, spawnPoint)
+		TombDungeon.generate(world, rand, entrancePoint)
 		
 		return TerritoryGenerationInfo(spawnPoint)
 	}
 	
 	private object EntranceCave{
-		private const val RADIUS_XZ = 65
-		private const val RADIUS_Y = 35
+		const val RADIUS_XZ = 65
+		const val RADIUS_Y = 35
+		
 		private const val EXTRA_ELEVATION = 2
 		
 		fun generate(world: SegmentedWorld, rand: Random, size: Size): BlockPos{
@@ -71,10 +89,10 @@ object Generator_ForgottenTombs : ITerritoryGenerator{
 					continue
 				}
 				
-				val centerPos = PosXZ(ellipsoidCenter.add(chunkX * 20, 0, chunkZ * 20))
+				val centerXZ = ellipsoidCenter.add(chunkX * 20, 0, chunkZ * 20).xz
 				
 				for(attempt in 1..10){
-					if (Stalactite.generate(world, rand, size, centerPos.add(rand.nextInt(-9, 9), rand.nextInt(-9, 9)))){
+					if (Stalactite.generate(world, rand, size, centerXZ.add(rand.nextInt(-9, 9), rand.nextInt(-9, 9)))){
 						break
 					}
 				}
@@ -214,6 +232,132 @@ object Generator_ForgottenTombs : ITerritoryGenerator{
 				
 				PASS.generate(world, bounds)
 			}
+		}
+	}
+	
+	private object TombDungeonEntrance{
+		private const val CUT_WIDTH_FULL = 8
+		private const val CUT_WIDTH_DOOR = 4
+		private const val CUT_HEIGHT = 5
+		
+		private val FULL_X_OFFSETS = -CUT_WIDTH_FULL..CUT_WIDTH_FULL
+		private val DOOR_X_OFFSETS = -CUT_WIDTH_DOOR..CUT_WIDTH_DOOR
+		private val DOOR_Y_OFFSETS = 0 until CUT_HEIGHT
+		
+		fun generate(world: SegmentedWorld, rand: Random, portal: BlockPos): BlockPos{
+			val minOffset = EntranceCave.RADIUS_XZ * 2 / 5
+			val maxOffset = EntranceCave.RADIUS_XZ
+			
+			val center = portal.up()
+			
+			val firstCut = center.offsetUntil(NORTH, minOffset..maxOffset){
+				FULL_X_OFFSETS.any { offX -> !world.isAir(it.east(offX)) }
+			} ?: center.north(minOffset)
+			
+			val entranceCut = center.offsetUntil(NORTH, (firstCut.z - center.z)..maxOffset){
+				DOOR_X_OFFSETS.all { offX -> DOOR_Y_OFFSETS.all { offY -> !world.isAir(it.east(offX).up(offY)) } }
+			} ?: center.north(maxOffset)
+			
+			val cutDistance = abs(entranceCut.z - firstCut.z)
+			
+			generateTerraforming(world, firstCut, entranceCut, cutDistance)
+			generateEntrance(world, rand, entranceCut, cutDistance)
+			
+			return entranceCut
+		}
+		
+		private fun generateTerraforming(world: SegmentedWorld, firstCut: BlockPos, entranceCut: BlockPos, cutDistance: Int){
+			world.placeCube(firstCut.add(-CUT_WIDTH_DOOR, 0, 0), entranceCut.add(CUT_WIDTH_DOOR, CUT_HEIGHT, 1), Air)
+			
+			for(mp in intArrayOf(-1, 1)){
+				world.placeCube(firstCut.add(mp * (CUT_WIDTH_DOOR + 1), 0, 0), entranceCut.add(mp * (CUT_WIDTH_DOOR + 1), CUT_HEIGHT, 2), Air)
+				world.placeCube(firstCut.add(mp * (CUT_WIDTH_DOOR + 2), 0, 0), entranceCut.add(mp * (CUT_WIDTH_DOOR + 2), CUT_HEIGHT, 3), Air)
+				
+				val end1 = entranceCut.add(mp * (CUT_WIDTH_DOOR + 3), 0, 4).offsetUntil(SOUTH, 0..(cutDistance - 3), world::isAir) ?: continue
+				world.setAir(end1.north(1))
+				world.setAir(end1.north(2))
+				
+				for(y in 1 until CUT_HEIGHT){
+					val endAbove = entranceCut.add(mp * (CUT_WIDTH_DOOR + 3), y, 4).offsetUntil(SOUTH, 0..(cutDistance - 3), world::isAir) ?: break
+					world.setAir(endAbove.north())
+				}
+				
+				val end2 = entranceCut.add(mp * (CUT_WIDTH_DOOR + 4), 0, 5).offsetUntil(SOUTH, 0..(cutDistance - 4), world::isAir) ?: continue
+				world.setAir(end2.north())
+			}
+		}
+		
+		private fun generateEntrance(world: SegmentedWorld, rand: Random, doorCenter: BlockPos, cutDistance: Int){
+			world.placeCube(doorCenter.add(-2, -1, 0), doorCenter.add(-2, 2, 0), TombDungeonPieces.PALETTE_ENTRY_FANCY_WALL)
+			world.placeCube(doorCenter.add(2, -1, 0), doorCenter.add(2, 2, 0), TombDungeonPieces.PALETTE_ENTRY_FANCY_WALL)
+			world.placeCube(doorCenter.add(-2, 3, 0), doorCenter.add(2, 3, 0), TombDungeonPieces.PALETTE_ENTRY_FANCY_WALL)
+			world.placeCube(doorCenter.add(-1, 0, 0), doorCenter.add(1, 2, 0), Air)
+			
+			for(pos in doorCenter.add(-CUT_WIDTH_DOOR - 3, -1, 0).allInBoxMutable(doorCenter.add(CUT_WIDTH_DOOR + 3, -1, cutDistance / 2))){
+				if (world.isAir(pos.up()) && !world.isAir(pos)){
+					world.setBlock(pos, ModBlocks.DUSTY_STONE)
+				}
+			}
+			
+			val floorEdge = doorCenter.add(0, -1, 1 + (cutDistance / 2))
+			
+			val cosOffset1 = rand.nextFloat(0.0, PI)
+			val cosOffset2 = cosOffset1 + (PI / 2) + rand.nextFloat(PI / 4, PI / 3)
+			
+			for(offsetX in -CUT_WIDTH_DOOR..CUT_WIDTH_DOOR){
+				val height = (abs(cos((offsetX / 2.0) + cosOffset1)) + abs(cos((offsetX / 1.2) + cosOffset2))).roundToInt()
+				
+				if (height > 0){
+					val firstPos = floorEdge.east(offsetX)
+					world.placeCube(firstPos, firstPos.south(height - 1), Single(ModBlocks.DUSTY_STONE))
+				}
+			}
+			
+			repeat(6){
+				for(attempt in 1..25){
+					val offsetX = rand.nextInt(3, CUT_WIDTH_FULL) * (if (rand.nextBoolean()) 1 else -1)
+					var randomPos = doorCenter.add(offsetX, 0, rand.nextInt(1, cutDistance - 3))
+					
+					if (!world.isAir(randomPos)){
+						if (rand.nextBoolean()){
+							continue
+						}
+						else{
+							randomPos = randomPos.up()
+						}
+					}
+					
+					if (world.isAir(randomPos) && Facing4.count { !world.isAir(randomPos.offset(it)) } < 2){
+						world.placeBlock(randomPos, TombDungeonPieces.PALETTE_ENTRY_PLAIN_WALL_CEILING)
+						
+						if (rand.nextInt(3) != 0){
+							world.setState(randomPos.up(), when(rand.nextInt(8)){
+								0 -> ModBlocks.DUSTY_STONE.defaultState
+								in 1..3 -> ModBlocks.DUSTY_STONE_BRICK_SLAB.defaultState
+								in 4..6 -> ModBlocks.DUSTY_STONE_BRICK_STAIRS.withFacing(rand.nextItem(Facing4))
+								else -> TombDungeonPieces.PALETTE_ENTRY_PLAIN_WALL_CEILING.pick(rand)
+							})
+						}
+						
+						break
+					}
+				}
+			}
+		}
+	}
+	
+	private object TombDungeon{
+		fun generate(world: SegmentedWorld, rand: Random, entrance: BlockPos){
+			for(attempt in 1..1000){
+				val build = TombDungeonBuilder.build(rand)
+				
+				if (build != null){
+					build.generate(OffsetStructureWorld(world, entrance.up().subtract(TombDungeonBuilder.ENTRANCE_POS).subtract(TombDungeonStart.size.centerPos)))
+					return
+				}
+			}
+			
+			HEE.log.error("[Generator_ForgottenTombs] failed all attempts at generating Tomb Dungeon")
 		}
 	}
 }
