145526a2c50491613b9be49df3db7dcd9e85310f
chylex <contact@chylex.com>
2019-09-16 19:27:20 +0200

    Log error in LootChestStructureTrigger if applied to invalid tile entity

--- src/main/java/chylex/hee/game/world/structure/trigger/LootChestStructureTrigger.kt
+++ src/main/java/chylex/hee/game/world/structure/trigger/LootChestStructureTrigger.kt
@@ -1,7 +1,9 @@
 package chylex.hee.game.world.structure.trigger
+import chylex.hee.HEE
 import chylex.hee.game.world.structure.IStructureTrigger
 import chylex.hee.game.world.util.Transform
 import chylex.hee.system.util.getTile
+import net.minecraft.tileentity.TileEntity
 import net.minecraft.tileentity.TileEntityLockableLoot
 import net.minecraft.util.ResourceLocation
 import net.minecraft.util.math.BlockPos
@@ -9,6 +11,13 @@ import net.minecraft.world.World
 
 class LootChestStructureTrigger(private val resource: ResourceLocation, private val seed: Long) : IStructureTrigger{
 	override fun realize(world: World, pos: BlockPos, transform: Transform){
-		pos.getTile<TileEntityLockableLoot>(world)?.setLootTable(resource, seed)
+		val tile = pos.getTile<TileEntityLockableLoot>(world)
+		
+		if (tile == null){
+			HEE.log.error("[LootChestStructureTrigger] failed adding loot table $resource, invalid tile entity type: ${pos.getTile<TileEntity>(world)?.javaClass?.simpleName}")
+		}
+		else{
+			tile.setLootTable(resource, seed)
+		}
 	}
 }
