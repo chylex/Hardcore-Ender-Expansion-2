72b377349c2afb05e7f8ef60dd823da2bd25129a
chylex <contact@chylex.com>
2019-10-13 11:44:02 +0200

    Tweak IBlockPlacer to only return true on block change & add documentation

--- src/main/java/chylex/hee/game/world/generation/IBlockPlacer.kt
+++ src/main/java/chylex/hee/game/world/generation/IBlockPlacer.kt
@@ -3,13 +3,16 @@ import net.minecraft.block.Block
 import net.minecraft.util.math.BlockPos
 
 interface IBlockPlacer{
+	/**
+	 * Attempts to place a block at the specified location. Returns true if a block was changed.
+	 */
 	fun place(world: SegmentedWorld, pos: BlockPos): Boolean
 	
 	// Implementations
 	
 	open class BlockPlacer(private val fill: Block) : IBlockPlacer{
 		override fun place(world: SegmentedWorld, pos: BlockPos): Boolean{
-			if (!world.isInside(pos)){
+			if (!world.isInside(pos) || world.getBlock(pos) === fill){
 				return false
 			}
 			
@@ -19,6 +22,12 @@ interface IBlockPlacer{
 	}
 	
 	open class BlockReplacer(private val fill: Block, private val replace: Block) : IBlockPlacer{
+		init{
+			if (fill === replace){
+				throw IllegalArgumentException("replacing a block only by itself makes no sense")
+			}
+		}
+		
 		override fun place(world: SegmentedWorld, pos: BlockPos): Boolean{
 			if (!world.isInside(pos) || world.getBlock(pos) !== replace){
 				return false
