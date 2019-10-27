170383b13334050457eecc1a45afd1008e39eb2c
chylex <contact@chylex.com>
2019-10-27 18:02:52 +0100

    Update custom smoke particle to allow setting a custom lifespan

--- src/main/java/chylex/hee/game/particle/ParticleSmokeCustom.kt
+++ src/main/java/chylex/hee/game/particle/ParticleSmokeCustom.kt
@@ -1,5 +1,5 @@
 package chylex.hee.game.particle
-import chylex.hee.game.particle.data.ParticleDataColorScale
+import chylex.hee.game.particle.data.ParticleDataColorLifespanScale
 import chylex.hee.game.particle.spawner.IParticleMaker
 import chylex.hee.system.migration.forge.Side
 import chylex.hee.system.migration.forge.Sided
@@ -13,23 +13,29 @@ import net.minecraft.client.particle.ParticleSmokeNormal
 import net.minecraft.world.World
 import java.util.Random
 
-object ParticleSmokeCustom : IParticleMaker<ParticleDataColorScale>{
+object ParticleSmokeCustom : IParticleMaker<ParticleDataColorLifespanScale>{
 	private val rand = Random()
 	
 	@Sided(Side.CLIENT)
-	override fun create(world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double, data: ParticleDataColorScale?): Particle{
+	override fun create(world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double, data: ParticleDataColorLifespanScale?): Particle{
 		return Instance(world, posX, posY, posZ, motX, motY, motZ, data ?: DEFAULT_DATA.generate(rand))
 	}
 	
 	fun Data(
 		color: IRandomColor = DefaultColor,
+		lifespan: IntRange,
 		scale: Float
-	) = ParticleDataColorScale.Generator(color, scale..scale)
+	) = ParticleDataColorLifespanScale.Generator(color, lifespan, scale..scale)
+	
+	fun Data(
+		color: IRandomColor = DefaultColor,
+		scale: Float
+	) = ParticleDataColorLifespanScale.Generator(color, DEFAULT_LIFESPAN, scale..scale)
 	
 	fun Data(
 		color: IntColor,
 		scale: Float
-	) = ParticleDataColorScale.Generator(IRandomColor.Static(color), scale..scale)
+	) = ParticleDataColorLifespanScale.Generator(IRandomColor.Static(color), DEFAULT_LIFESPAN, scale..scale)
 	
 	private object DefaultColor : IRandomColor{
 		override fun next(rand: Random): IntColor{
@@ -38,10 +44,11 @@ object ParticleSmokeCustom : IParticleMaker<ParticleDataColorScale>{
 	}
 	
 	private val DEFAULT_DATA = Data(scale = 1F)
+	private val DEFAULT_LIFESPAN = (-1)..(-1)
 	
 	@Sided(Side.CLIENT)
 	private class Instance(
-		world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double, data: ParticleDataColorScale
+		world: World, posX: Double, posY: Double, posZ: Double, motX: Double, motY: Double, motZ: Double, data: ParticleDataColorLifespanScale
 	) : ParticleSmokeNormal(
 		world, posX, posY, posZ, motX, motY, motZ, data.scale
 	){
@@ -51,6 +58,10 @@ object ParticleSmokeCustom : IParticleMaker<ParticleDataColorScale>{
 			particleRed = color.red / 255F
 			particleGreen = color.green / 255F
 			particleBlue = color.blue / 255F
+			
+			if (data.lifespan != -1){
+				particleMaxAge = data.lifespan
+			}
 		}
 	}
 }
