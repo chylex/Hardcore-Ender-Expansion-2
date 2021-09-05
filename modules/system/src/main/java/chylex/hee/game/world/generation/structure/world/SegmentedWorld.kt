858fa672ec50f94c71bcb5f2fdeeadfe6484c0c0
chylex <contact@chylex.com>
2021-09-05 07:33:25 +0200

    Optimize segmented world generation (reduce allocations in getState/setState)

--- modules/system/src/main/java/chylex/hee/game/world/generation/structure/world/SegmentedWorld.kt
+++ modules/system/src/main/java/chylex/hee/game/world/generation/structure/world/SegmentedWorld.kt
@@ -9,55 +9,64 @@ import chylex.hee.game.world.util.Transform
 import chylex.hee.util.math.Pos
 import chylex.hee.util.math.Size
 import chylex.hee.util.math.ceilToInt
-import chylex.hee.util.math.component1
-import chylex.hee.util.math.component2
-import chylex.hee.util.math.component3
 import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
 import net.minecraft.util.math.BlockPos
 import java.util.Random
 
-open class SegmentedWorld(override val rand: Random, val worldSize: Size, private val segmentSize: Size, defaultSegmentFactory: (Size) -> ISegment) : IStructureWorld {
+open class SegmentedWorld(override val rand: Random, val worldSize: Size, segmentSize: Size, defaultSegmentFactory: (Size) -> ISegment) : IStructureWorld {
+	private val segmentSizeX = segmentSize.x
+	private val segmentSizeY = segmentSize.y
+	private val segmentSizeZ = segmentSize.z
+	
 	private val segmentCounts = Size(
-		(worldSize.x.toFloat() / segmentSize.x).ceilToInt(),
-		(worldSize.y.toFloat() / segmentSize.y).ceilToInt(),
-		(worldSize.z.toFloat() / segmentSize.z).ceilToInt()
+		(worldSize.x.toFloat() / segmentSizeX).ceilToInt(),
+		(worldSize.y.toFloat() / segmentSizeY).ceilToInt(),
+		(worldSize.z.toFloat() / segmentSizeZ).ceilToInt()
 	)
 	
 	private val segments = Array(segmentCounts.x * segmentCounts.y * segmentCounts.z) { defaultSegmentFactory(segmentSize) }
 	private val triggers = mutableListOf<Pair<BlockPos, IStructureTrigger>>()
 	
-	private fun mapPos(pos: BlockPos): Pair<Int, BlockPos>? {
-		if (!isInside(pos)) {
-			HEE.log.warn("[SegmentedWorld] attempted to access position outside bounds: $pos is outside $worldSize")
-			
-			if (HEE.debug) {
-				Thread.dumpStack()
-			}
-			
-			return null
-		}
-		
-		val (x, y, z) = pos
-		val (sizeX, sizeY, sizeZ) = segmentSize
-		
-		val segmentIndex = index(x / sizeX, y / sizeY, z / sizeZ, segmentCounts)
-		val segmentOffset = Pos(x % sizeX, y % sizeY, z % sizeZ)
-		
-		return Pair(segmentIndex, segmentOffset)
+	private fun getSegmentIndex(pos: BlockPos): Int {
+		return index(pos.x / segmentSizeX, pos.y / segmentSizeY, pos.z / segmentSizeZ, segmentCounts)
+	}
+	
+	private fun getSegmentOffset(pos: BlockPos): BlockPos {
+		return Pos(pos.x % segmentSizeX, pos.y % segmentSizeY, pos.z % segmentSizeZ)
 	}
 	
 	fun isInside(pos: BlockPos): Boolean {
 		return pos.x in 0..worldSize.maxX && pos.y in 0..worldSize.maxY && pos.z in 0..worldSize.maxZ
 	}
 	
+	private fun warnNotInside(pos: BlockPos) {
+		HEE.log.warn("[SegmentedWorld] attempted to access position outside bounds: $pos is outside $worldSize")
+		
+		if (HEE.debug) {
+			Thread.dumpStack()
+		}
+	}
+	
 	override fun getState(pos: BlockPos): BlockState {
-		val (segmentIndex, segmentOffset) = mapPos(pos) ?: return Blocks.AIR.defaultState
+		if (!isInside(pos)) {
+			warnNotInside(pos)
+			return Blocks.AIR.defaultState
+		}
+		
+		val segmentIndex = getSegmentIndex(pos)
+		val segmentOffset = getSegmentOffset(pos)
 		return segments[segmentIndex].getState(segmentOffset)
 	}
 	
 	override fun setState(pos: BlockPos, state: BlockState) {
-		val (segmentIndex, segmentOffset) = mapPos(pos) ?: return
+		if (!isInside(pos)) {
+			warnNotInside(pos)
+			return
+		}
+		
+		val segmentIndex = getSegmentIndex(pos)
+		val segmentOffset = getSegmentOffset(pos)
 		segments[segmentIndex] = segments[segmentIndex].withState(segmentOffset, state)
 	}
 	
